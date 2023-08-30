d64ef8f6a66fa5bd23f6a9ea9bc15aeb32229a1e
mblancmcs <mblancm.cs@gmail.com>
2023-08-30 11:04:55 -0300

    first commit

---
+++ src/main/java/biz/blancoder/ecommercelivraria/EcommerceLivrariaApplication.java
@@ -0,0 +1,13 @@
+package biz.blancoder.ecommercelivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceLivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/blancoder/ecommercelivraria/EcommerceLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.blancoder.ecommercelivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
